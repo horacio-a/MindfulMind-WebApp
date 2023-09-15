270778a42acebc3e32d63eaa5808fd15370fae2f
Horacio Albornoz <horaciomatiasalbornoz@gmail.com>
2023-09-15 16:13:31 -0300

    <feat> [components\Main.js]: main setion

---
+++ components/Main.js
@@ -0,0 +1,29 @@
+import axios from "axios"
+import { useEffect, useState } from "react"
+import Routine from "./MainPages/Routine"
+import Calendar from "./MainPages/Calendar"
+import Text from "./MainPages/Text"
+
+export default function Main({ sidebar, routineData, setroutineData, calendarData, setcalendarData, TextData, setTextData, loading, setloading, page, refreshData }) {
+
+    return (
+        <div className={`MainContent ${sidebar ? 'Active' : 'Desactive'}`}>
+
+            {
+                page === 'Routine'
+                    ? <Routine routineData={routineData} setroutineData={setroutineData} loading={loading} setloading={setloading} />
+                    : <></>
+            }
+            {
+                page === 'Calendar'
+                    ? <Calendar calendarData={calendarData} setcalendarData={setcalendarData} loading={loading} setloading={setloading} refreshData={refreshData} />
+                    : <></>
+            }
+            {
+                page === 'Text'
+                    ? <Text TextData={TextData} setTextData={setTextData} loading={loading} setloading={setloading} />
+                    : <></>
+            }
+        </div>
+    )
+}
